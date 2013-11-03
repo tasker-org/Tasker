d2eb2b7782c2468fb9fcb5afc279273eda77604d
JSifalda <sifalda.jiri@gmail.com>
2013-11-03 08:54:19 +0100

    Modify condition

--- Tasker/Output/Colors.php
+++ Tasker/Output/Colors.php
@@ -27,7 +27,7 @@ class Colors
 	 */
 	public static function getColored($message, $type = IWriter::NONE)
 	{
-		if($type !== IWriter::NONE || !Environment::isUnix()) {
+		if($type != IWriter::NONE && Environment::isUnix()) {
 			$pattern = "\033[__!COLOR__m__!MESSAGE__\033[0m";
 			return str_replace('__!MESSAGE__', (string) $message, str_replace('__!COLOR__', static::getColor($type), $pattern));
 		}
